024f6266dbd92d57e81b6a9403e0c788246a68ee
joseasenz <joseasenz@gmail.com>
2023-11-12 17:29:49 +0000

    Add CUBE project M3_T1
    Project implementing basic functionality for
    AD, UART and GPIO

---
+++ M3_T1/Core/Inc/console.h
@@ -0,0 +1,68 @@
+/*
+ * console.h
+ *
+ *  Created on: Nov 10, 2023
+ *      Author: JOSE
+ */
+
+#ifndef INC_CONSOLE_H_
+#define INC_CONSOLE_H_
+
+
+// INCLUDES
+#include "stm32f1xx_hal.h"
+#include <string.h>
+#include "stdbool.h"
+#include "usart.h"
+
+// TYPE DEFINITIONS
+typedef struct
+{
+
+  uint16_t AdcValue;
+  uint8_t SpiValue;
+
+} CONSOLE_DataStreamType;
+
+// DELCARATIONS
+extern CONSOLE_DataStreamType ConsoleDataStream;
+
+
+// PROTOTYPES
+void ConsoleDebugDataFormat();
+void ConsoleDebugDataSend();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* INC_CONSOLE_H_ */
---
+++ M3_T1/Core/Src/console.c
@@ -0,0 +1,32 @@
+/*
+ * console.c
+ *
+ *  Created on: Nov 10, 2023
+ *      Author: JOSE
+ */
+
+#include "console.h"
+
+
+CONSOLE_DataStreamType ConsoleDataStream;
+
+bool DebugOutputEnabled = false;
+
+
+void ConsoleDebugDataFormat()
+{
+	sprintf(UartTxBuffer, "#DBG: %d \r\n", ConsoleDataStream.AdcValue);
+}
+
+
+
+void ConsoleDebugDataSend()
+{
+	if(DebugOutputEnabled)
+	{
+		ConsoleDebugDataFormat();
+		HAL_UART_Transmit(&huart3, UartTxBuffer, strlen(UartTxBuffer), 100);
+	}
+}
+
+
